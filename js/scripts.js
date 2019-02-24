84f5519a11236a42c2944622a39c7d8104254adf
Dickson <dicksonparlma@gmail.com>
2019-02-24 15:13:02 +0300

    added functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
     var package=$("#category").val();
     $("#output").text("Your"+" "+package+"Photoshoot has been scheduled on"+" "+choice + " " + ".We'll contact you through " + " " + customer);
   });
+  $("#lensflare").click(function(){
+    $("#Edition2").slideUp();
+    $("#Edition1").slideToggle();
+  });
+  $("#photoboom").click(function(){
+    $("#Edition1").slideUp();
+    $("#Edition2").slideToggle();
+  });
 });
